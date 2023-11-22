8ff802ec680465e00a6b34b0f14b6d44c6f24ec3
anas0 <ashraful0a@gmail.com>
2023-11-23 02:05:50 +0600

    make controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from "express";
+import User from "../models/user.model";
+
+const createUser = async (req: Request, res: Response) => {
+    try {
+        const userData = req.body;
+        
+        const result = await User.create(userData);
+        res.status(201).json({
+            status: "success",
+            message: "User created successfully",
+            data: result
+        })
+
+    } catch (error: any) {
+        console.log(error);
+        res.status(500).json({
+            status: "fail",
+            message: error.message || "Something went wrong"
+        })
+    }
+}
+
+export const userController = { createUser }
