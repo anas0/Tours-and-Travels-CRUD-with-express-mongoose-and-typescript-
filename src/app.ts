7af386a9a29ab15b1c9e30e8ac6d6d5f30f85eb3
anas0 <ashraful0a@gmail.com>
2023-11-22 21:48:06 +0600

    express router added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,23 @@
 import express, { Application, Request, Response } from 'express'
+
 const app: Application = express()
+
+const userRoute = express.Router()
+
+userRoute.get('/all-users', (req: Request, res: Response) =>{
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to Touts and Travels',
+    data: [
+      {
+        name: "bappy",
+        email: "bappy@g.com"
+      }
+    ]
+  })
+})
+
+app.use('/api/v1/users', userRoute)
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
@@ -8,26 +26,26 @@ app.get('/', (req: Request, res: Response) => {
   })
 })
 
-app.get('/api/v1/users', (req: Request, res: Response) => {
-  const user = [
-    {
-      id: 1,
-      name: 'bismi',
-      email: 'bismi@gmail.com'
-    },
-    {
-      id: 2,
-      name: 'akib',
-      email: 'akib@gmail.com'
-    }
-  ]
+// app.get('/api/v1/users', (req: Request, res: Response) => {
+//   const user = [
+//     {
+//       id: 1,
+//       name: 'bismi',
+//       email: 'bismi@gmail.com'
+//     },
+//     {
+//       id: 2,
+//       name: 'akib',
+//       email: 'akib@gmail.com'
+//     }
+//   ]
 
-  res.status(200).json({
-    status: 'success',
-    message: 'User fetched successfully',
-    data: user
-  })
-})
+//   res.status(200).json({
+//     status: 'success',
+//     message: 'User fetched successfully',
+//     data: user
+//   })
+// })
 
 
 export default app
